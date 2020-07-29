69c0e06cbd4b0422371b8b5f6f03d72732f052cb
aland1013 <adam.land@gmail.com>
2020-07-28 20:59:00 -0400

    finished helper methods

---
+++ schemes/scheme-model.js
@@ -0,0 +1,52 @@
+const db = require('../data/db-config');
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
+
+function find() {
+  return db('schemes');
+}
+
+function findById(id) {
+  return db('schemes').where({ id }).first();
+}
+
+function findSteps(id) {
+  return db('schemes')
+    .join('steps', 'steps.scheme_id', 'schemes.id')
+    .select(
+      'schemes.id',
+      'schemes.scheme_name',
+      'steps.step_number',
+      'steps.instructions'
+    )
+    .where({ scheme_id: id })
+    .orderBy('steps.step_number');
+}
+
+function add(scheme) {
+  return db('schemes')
+    .insert(scheme)
+    .then((ids) => {
+      return findById(ids[0]);
+    });
+}
+
+function update(changes, id) {
+  return db('schemes')
+    .where({ id })
+    .update(changes)
+    .then((count) => {
+      return findById(id);
+    });
+}
+
+function remove(id) {
+  return db('schemes').where({ id }).del();
+}
